c18c99163d709e589cd5efca5b8c9217bce54d49
Che <che.android@gmail.com>
2019-04-23 12:08:21 +0100

    Adds caching to OkHttp client

--- app/src/main/java/che/codes/posts/di/modules/NetworkModule.kt
+++ app/src/main/java/che/codes/posts/di/modules/NetworkModule.kt
@@ -1,14 +1,19 @@
 package che.codes.posts.di.modules
 
+import che.codes.posts.PostsApplication
 import dagger.Module
 import dagger.Provides
+import okhttp3.Cache
 import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 @Module
 class NetworkModule {
+    private val cacheSize: Long = 5 * 1024 * 1024
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient = OkHttpClient()
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().cache(Cache(PostsApplication.instance.cacheDir, cacheSize)).build()
+    }
 }
